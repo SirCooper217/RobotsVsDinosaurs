c5878b7f02f0a5661f1feceb892cba6f4b57a510
SirCooper217 <ryanjc1597@yahoo.com>
2022-11-17 17:07:26 -0600

    Created Battlefield class

---
+++ battlefield.py
@@ -0,0 +1,16 @@
+class battlefield:
+    def __init__(self):
+        self.dinosaur= " "
+        self.robot= " "
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        print("\nIt's a GOD DANG Dinosaur vs Robot fight! \nLet's get ready to RUMBLLLLLE\n")
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
