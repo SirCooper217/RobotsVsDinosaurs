2e33a8c023ed255dbb58d356b0f0e64d5bcb5111
SirCooper217 <ryanjc1597@yahoo.com>
2022-11-17 16:52:17 -0600

    Created Dinosaur class

---
+++ dinosaur.py
@@ -0,0 +1,9 @@
+class dinosaur:
+    def __init__(self, name, attack_power):
+        self.name= " "
+        self.attack_power= [ ]
+        self.health= [ ]
+
+    def attack(self, robot):
+        pass
+    
