7dbec7c83b7eb5c8dcf4108a5a57cd062472d99b
SirCooper217 <ryanjc1597@yahoo.com>
2022-11-23 15:40:12 -0600

    Final code, polished and debugged. final ver.

--- battlefield.py
+++ battlefield.py
@@ -20,18 +20,20 @@ class Battlefield:
     def battle_phase(self):
         while self.dinosaur.health> 0 and self.robot.health> 0:
             self.dinosaur.attack(self.robot)
-            print(f"\nDino attacks Robo for {self.dinosaur.attack_power} damage! \n Robo has {self.robot.health} remaining!")
+            print(f"\n{self.dinosaur.name} attacks {self.robot.name} for {self.dinosaur.attack_power} damage! \n {self.robot.name} has {self.robot.health} remaining!")
             if self.robot.health> 0:
                 self.robot.attack(self.dinosaur)
-                print(f"\nRobo attacks Dino for {self.robot.active_weapon.attack_power} damage! \n Dino has {self.dinosaur.health} remaining!")
+                print(f"\n{self.robot.name} attacks {self.dinosaur.name} for {self.robot.active_weapon.attack_power} damage! \n {self.dinosaur.name} has {self.dinosaur.health} remaining!")
         if self.dinosaur.health<= 0:
             self.winner= self.robot.name
-            print("Robo time")
+            self.loser= self.dinosaur.name
+            print(f"{self.winner} drove {self.loser} to EXTINCTION!!")
         else:
             self.winner= self.dinosaur.name
-            print("Dino time")
+            self.loser= self.robot.name
+            print(f"{self.winner} drove {self.loser} to EXTINCTION!!")
         pass
 
     def display_winner(self):
-        print(f"\nIt's a GOD DANG Dinosaur vs Robot fight! \nAnd it looks like {self.winner} wins!\n")
+        print(f"\nIt's a GOD DANG Dinosaur vs Robot fight! \nAnd it looks like {self.winner} wins! Better luck next time, {self.loser}!\n")
         pass
